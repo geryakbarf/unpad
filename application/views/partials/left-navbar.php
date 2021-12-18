35612ea1efd13aa5ba1698eaf3387f1942701393
Gery Akbar Fauzi <geryakbarfauzi@gmail.com>
2021-12-18 13:11:21 +0700

    - Menampilkan bank kata dari library (sudah modifikasi library)

--- application/views/partials/left-navbar.php
+++ application/views/partials/left-navbar.php
@@ -1,7 +1,7 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="/admin" class="brand-link">
+    <a href="<?php echo base_url()."Dashboard";?>" class="brand-link">
         <img src="<?= base_url('assets/'); ?>img/logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
              style="opacity: .8">
         <span class="brand-text font-weight-light">Unpad</span>
