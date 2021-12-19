68376155a31d463ae059d3bdf055c72001fc4da0
Gery Akbar Fauzi <geryakbarfauzi@gmail.com>
2021-12-19 17:00:03 +0700

    Mengubah logo pada halaman dashboard

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -135,7 +135,7 @@ usort( $trend_penerimaan, function( $a, $b) {
 
   <!-- Preloader -->
   <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+    <img class="animation__shake" src="<?= base_url('assets/'); ?>img/logo.png" alt="AdminLTELogo" height="60" width="60">
   </div>
 
   <!--Navbar -->
